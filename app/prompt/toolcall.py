2ec579e58d5237697405ebba081444c9f72265f6
liangxinbing <1580466765@qq.com>
2025-01-10 14:22:33 +0800

    update prompt of toolcall.py

--- app/prompt/toolcall.py
+++ app/prompt/toolcall.py
@@ -1,5 +1,3 @@
-# SYSTEM_PROMPT = "You are an agent that can execute tool calls"
-SYSTEM_PROMPT = ""
+SYSTEM_PROMPT = "You are an agent that can execute tool calls"
 
-# NEXT_STEP_PROMPT = "If you want to stop interaction, use `finish` tool/function call."
-NEXT_STEP_PROMPT = ""
+NEXT_STEP_PROMPT = "If you want to stop interaction, use `finish` tool/function call."
