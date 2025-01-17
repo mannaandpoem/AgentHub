1e7296c38ffa0ec11c7311a00c4c05829f93bd11
liangxinbing <1580466765@qq.com>
2025-01-17 11:34:35 +0800

    update tool and requirements.txt

--- app/utils/parse_oh_aci_output.py
+++
@@ -1,8 +0,0 @@
-import json
-import re
-
-
-def parse_oh_aci_output(tool_output, return_string=True):
-    pattern = r"<oh_aci_output_.*?>(.*?)</oh_aci_output_"
-    json_data = json.loads(re.search(pattern, tool_output, re.DOTALL).group(1).strip())
-    return json_data["formatted_output_and_error"] if return_string else json_data
